898558e544df1ead3a5070d08a68b560c505d04c
swimer-girl <swimermail@gmail.com>
2023-12-16 13:51:35 +0300

    11 task

--- init/js/main.js
+++ init/js/main.js
@@ -1,7 +1,21 @@
-import './map.js';
 import './statepage.js';
 import './form.js';
 import { createMarker } from './map.js';
 import { getOffer } from './data.js';
 
 getOffer(createMarker());
+const getData = async (onSuccess) => {
+  try {
+    const response = await fetch ('https://25.javascript.pages.academy/keksobooking/data');
+
+    if (!response.ok) {
+      throw new Error('Не удалось загрузить обьявления');
+    }
+    const offers = await response.json();
+    onSuccess(offers);
+  } catch (error) {
+    //onFail(error.message);
+  }
+};
+
+getData();
